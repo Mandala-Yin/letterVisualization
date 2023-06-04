7e6f672ebff1ea124007b33f6a9048b21b94135f
2367752205@qq.com <52498812+Orange1918@users.noreply.github.com>
2023-06-04 19:06:46 +0800

    clean

--- app.py
+++ app.py
@@ -3,31 +3,36 @@ import pandas as pd
 
 app = Flask(__name__)
 
-filterBy = {'通讯关系': False, '作者生年': True, '作者卒年': True}
-relationFtr = ['致书Y', '答Y书', '向Y致贺', '致Y啓', '代Y作文']
-birthYFtr = {'startY': 1500, 'endY': 1601} # 含左不含右
-deathYFtr = {'startY': 1500, 'endY': 1601} # 含左不含右
+data = pd.read_excel('data/letter.xlsx')
+original_data = data.to_dict(orient='records')
+filtered_data = original_data
 
-df = pd.read_excel('data/letter.xlsx')
-# df = pd.read_excel('data/test.xlsx')
-filtered_data = df.to_dict(orient='records')
-if filterBy['通讯关系']:
-    filtered_data = [item for item in filtered_data if item['通讯关系'] in relationFtr]
-if filterBy['作者生年']:
-    filtered_data = [item for item in filtered_data if not pd.isna(item['作者生年'])]
-    filtered_data = [item for item in filtered_data if birthYFtr['startY'] <= int(float(str(item['作者生年']).replace('≈', ''))) < birthYFtr['endY']]
-if filterBy['作者卒年']:
-    filtered_data = [item for item in filtered_data if not pd.isna(item['作者卒年'])]
-    filtered_data = [item for item in filtered_data if deathYFtr['startY'] <= int(float(str(item['作者卒年']).replace('≈', ''))) < deathYFtr['endY']]
+def filter():
+    filterBy = {'通讯关系': False, '作者生年': True, '作者卒年': True}
+    relationFtr = ['致书Y', '答Y书', '向Y致贺', '致Y啓', '代Y作文']
+    birthYFtr = {'startY': 1500, 'endY': 1601}  # 含左不含右
+    deathYFtr = {'startY': 1500, 'endY': 1601}  # 含左不含右
 
-# print(filtered_data)
-filtered_df = pd.DataFrame(filtered_data)
-filtered_df.to_excel('data/filtered_data.xlsx', index=False)
+    if filterBy['通讯关系']:
+        filtered_data = [item for item in filtered_data if item['通讯关系'] in relationFtr]
+    if filterBy['作者生年']:
+        filtered_data = [
+            item for item in filtered_data if not pd.isna(item['作者生年'])]
+        filtered_data = [item for item in filtered_data if birthYFtr['startY'] <= int(
+            float(str(item['作者生年']).replace('≈', ''))) < birthYFtr['endY']]
+    if filterBy['作者卒年']:
+        filtered_data = [
+            item for item in filtered_data if not pd.isna(item['作者卒年'])]
+        filtered_data = [item for item in filtered_data if deathYFtr['startY'] <= int(
+            float(str(item['作者卒年']).replace('≈', ''))) < deathYFtr['endY']]
 
-# @app.route('/')
-# def index():
-#     return render_template('/home.html', data=data)
+    filtered_df = pd.DataFrame(filtered_data)
+    # filtered_df.to_excel('data/filtered_data.xlsx', index=False)
+
+
+@app.route('/')
+def index():
+    return render_template('/home.html', data=original_data)
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=5000)
-    pass
+    app.run()
