d63e18d22b4bc060cb9c62a91fc8f605dc78adfc
2367752205@qq.com <52498812+Orange1918@users.noreply.github.com>
2023-06-04 16:19:59 +0800

    use the whole dataset

--- app.py
+++ app.py
@@ -3,15 +3,14 @@ import pandas as pd
 
 app = Flask(__name__)
 
+df = pd.read_excel('data/letter.xlsx')
+original_data = df.to_dict(orient='records')
+data = original_data
+
 @app.route('/')
 def index():
-    # 读取 Excel 文件
-    df = pd.read_excel('data/letter.xlsx')
-
-    # 将数据转换为字典列表
-    data = df.to_dict(orient='records')
-
     return render_template('/home.html', data=data)
 
 if __name__ == '__main__':
+
     app.run()
