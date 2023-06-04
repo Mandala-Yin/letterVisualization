d31c0ad3f71ed94d5d397f0c427d89eae68def07
SilentDeep <silent_deep@163.com>
2023-06-04 16:57:39 +0800

    filter

--- app.py
+++ app.py
@@ -3,15 +3,26 @@ import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    # 读取 Excel 文件
-    df = pd.read_excel('data/letter.xlsx')
+# filterBy = {'通讯关系': False, '作者生年': False, '作者卒年': False}
+relationFtr = ['致书Y', '答Y书', '向Y致贺', '致Y啓', '代Y作文']
+birthYFtr = {'startY': 1500, 'endY': 1601} # 含左不含右
+deathYFtr = {'startY': 1500, 'endY': 1601} # 含左不含右
 
-    # 将数据转换为字典列表
-    data = df.to_dict(orient='records')
+# df = pd.read_excel('data/letter.xlsx')
+df = pd.read_excel('data/test.xlsx')
+tmpData = df.to_dict(orient='records')
+filtered_data = [item for item in tmpData if item['通讯关系'] in relationFtr and 
+                 birthYFtr['startY'] <= item['作者生年'] < birthYFtr['endY'] and 
+                 deathYFtr['startY'] <= item['作者卒年'] < deathYFtr['endY']]
 
-    return render_template('/home.html', data=data)
+# print(filtered_data)
+filtered_df = pd.DataFrame(filtered_data)
+filtered_df.to_excel('data/filtered_data.xlsx', index=False)
+
+# @app.route('/')
+# def index():
+#     return render_template('/home.html', data=data)
 
 if __name__ == '__main__':
-    app.run()
+    # app.run(host='0.0.0.0', port=5000)
+    pass
